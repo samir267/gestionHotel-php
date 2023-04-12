b3f1ad8d8667e760f20ab44bb2c18186b23d3382
samir chemkhi <samirchemkhi09@gmail.com>
2023-04-12 21:53:52 +0100

    second commit

--- dashboard.php
+++ dashboard.php
@@ -193,30 +193,31 @@
       </thead>
       <tbody>
         
-          <?php
+         
+<?php
 require 'connection.php';
-$sql = "SELECT * FROM user";
-$result = $conn->query($sql);
+include 'userClass.php';
+$user=new User(null,"","","","","");
+$res=$user->__selection($conn);
 
-if ($result->rowCount() > 0){
-  while($row=$result->fetch()){
+while($row=$res->fetch()){
+  ?>
+<tr>
+          
+          <td><?php echo $row["nom"] ?></td>
+          <td><?php echo $row["password"] ?></td>
+          
+          <td>
 
-echo '<tr>
-<td>' .$row["username"].'</td>
-  <td>'.$row["password"].'</td>
+          <a href="#editEmployeeModal?id=<?php echo $row['id'] ?>" class="edit" >
+			<i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+            <a href="#deleteEmployeeModal" class="delete" data-toggle="modal">
+			<i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+          </td>
+        </tr>
+<?php
+  }
 
-
-  <td>
-    <a href="#editEmployeeModal" class="edit" data-toggle="modal">
-<i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-    <a href="#deleteEmployeeModal" class="delete" data-toggle="modal">
-<i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-  </td>
-';
-
-
-}
-}
 ?>
 
  
@@ -282,41 +283,7 @@ echo '<tr>
   </div>
 </div>
 <!-- Edit Modal HTML -->
-<div id="editEmployeeModal" class="modal fade">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <form>
-        <div class="modal-header">
-          <h4 class="modal-title">Edit Client</h4>
-          <button type="button" class="close" data-dismiss="modal" 
-		  aria-hidden="true">&times;</button>
-        </div>
-        <div class="modal-body">
-          <div class="form-group">
-            <label>Name</label>
-            <input type="text" class="form-control" required>
-          </div>
-          <div class="form-group">
-            <label>Email</label>
-            <input type="email" class="form-control" required>
-          </div>
-          <div class="form-group">
-            <label>Address</label>
-            <textarea class="form-control" required></textarea>
-          </div>
-          <div class="form-group">
-            <label>Phone</label>
-            <input type="text" class="form-control" required>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-          <input type="submit" class="btn btn-info" value="Save">
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
+
 
 
 
---
+++ userClass.php
@@ -0,0 +1,65 @@
+<?php
+require 'connection.php';
+class User {
+    private $id;
+    private $nom;
+    private $prenom;
+    private $email;
+    private $password;
+    private $telephone;
+
+    public function __construct($id, $nom, $prenom, $email, $password, $telephone) {
+        $this->id = $id;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->email = $email;
+        $this->password = $password;
+        $this->telephone = $telephone;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNom() {
+        return $this->nom;
+    }
+
+    public function getPrenom() {
+        return $this->prenom;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getTelephone() {
+        return $this->telephone;
+    }
+
+    public function __selection($conn){
+        $sql = $conn->prepare("SELECT * FROM user");
+        $sql->execute();
+        return $sql;
+
+    }
+
+
+
+    public function selection_id($conn,$id){
+        //requete parametrée
+        $st=$conn->prepare("select * from user where id=?");
+        $st->execute(array($id));
+        $row=$st->fetch();
+        $this->nom=$row['nom'];
+
+}
+}
+   
+
+
+?>
